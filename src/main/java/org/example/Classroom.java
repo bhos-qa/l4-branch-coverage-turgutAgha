a5c89ab1955f341e63124004ac1722c15b819f09
turgutAgha <turgut.aghashov.std@bhos.edu.az>
2022-10-01 14:51:55 +0400

    Classroom created

---
+++ src/main/java/org/example/Classroom.java
@@ -0,0 +1,54 @@
+package org.example;
+
+enum Gender{
+    MALE,
+    FEMALE
+}
+
+public class Classroom {
+    String studentName = "John Doe";
+    int age = 20;
+    double gpa = 95.5;
+    Gender gender = Gender.MALE;
+    String nationality = "Azerbaijani";
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+}
